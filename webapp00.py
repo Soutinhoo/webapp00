11fb64b9295de5e59758f6f77909fec31a369665
Soutinhoo <110978589+Soutinhoo@users.noreply.github.com>
2022-08-12 08:59:46 -0300

    Update webapp00.py

--- webapp00.py
+++ webapp00.py
@@ -4,10 +4,10 @@
 import streamlit as st
   
 # Use st.title("") para adicionar um TÍTULO ao seu Web app
-st.title("MEU 1º WEB APP STREAMLIT")
+st.title("AUTOCUST")
 
 # Use st.header("") para adicionar um CABEÇALHO ao seu Web app
-st.header("Cabeçalho")
+st.header("Custo do seu carro")
 
 # Use st.subheader("") para adicionar um SUB CABEÇALHO ao seu Web app
 st.subheader("Sub Cabeçalho")
